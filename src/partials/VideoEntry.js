5238c0bd88fa1826dd4b0d51fd65e0a062241ec3
Lukasz Kowalski <krowal@gmail.com>
2016-02-17 20:33:08 +0100

    active state for current path in submenu

--- src/partials/VideoEntry.js
+++ src/partials/VideoEntry.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class Entry extends Component{
+export default class VideoEntry extends Component{
     render(){
         return (
             <div>Im an entry, you can edit me</div>
--- src/partials/VideoMenu.js
+++ src/partials/VideoMenu.js
@@ -1,16 +1,28 @@
-import {Link} from 'react-router';
+import {Router, Link} from 'react-router';
 import React, {Component} from 'react';
+
+let menu = [
+    {
+        path:'/',
+        name:'Show library'
+    },
+    {
+        path:'/add',
+        name:'Add new Video'
+    }
+];
 
 export default class VideoMenu extends Component{
     render(){
         return(
             <ul className="nav nav-pills">
-                <li role="presentation">
-                    <Link to="/">Show library</Link>
-                </li>
-                <li role="presentation">
-                    <Link to="/add">Add new video</Link>
-                </li>
+                {menu.map(function(item){
+                    return (
+                        <li className={item.path == location.pathname ? "active" : ""} role="presentation">
+                            <Link to={item.path}>{item.name}</Link>
+                        </li>
+                    );
+                })}
             </ul>
         )
     }
